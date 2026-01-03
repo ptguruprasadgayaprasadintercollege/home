a752d1547faccbf1f132e34b0d5a72637dde01fe
ptguruprasadgayaprasadintercollege <pggic1234@gmail.com>
2026-01-03 20:32:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -78,55 +78,55 @@ function showGallery(type) {
 
     if (type === "classroom") {
         images = [
-            "/images/classroom1.jpg",
-            "/images/classroom2.jpg",
-            "/images/classroom3.jpg",
-            "/images/classroom4.jpg",
-            "/images/classroom5.jpg",
-            "/images/classroom6.jpg"
+            "images/classroom1.jpg",
+            "images/classroom2.jpg",
+            "images/classroom3.jpg",
+            "images/classroom4.jpg",
+            "images/classroom5.jpg",
+            "images/classroom6.jpg"
         ];
     } else if (type === "hostel") {
         images = [
-            "/images/hostel1.jpg",
-            "/images/hostel2.jpg",
-            "/images/hostel3.jpg",
-            "/images/hostel4.jpg",
-            "/images/hostel5.jpg",
-            "/images/hostel6.jpg"
+            "images/hostel1.jpg",
+            "images/hostel2.jpg",
+            "images/hostel3.jpg",
+            "images/hostel4.jpg",
+            "images/hostel5.jpg",
+            "images/hostel6.jpg"
         ];
     } else if (type === "playground") {
         images = [
-            "/images/playground1.jpg",
-            "/images/playground2.jpg",
-            "/images/playground3.jpg",
-            "/images/playground4.jpg",
-            "/images/playground5.jpg",
-            "/images/playground6.jpg"
+            "images/playground1.jpg",
+            "images/playground2.jpg",
+            "images/playground3.jpg",
+            "images/playground4.jpg",
+            "images/playground5.jpg",
+            "images/playground6.jpg"
         ];
     }
     else if (type === "labs") {
         images = [
-            "/images/labs1.jpg",
-            "/images/labs2.jpg",
-            "/images/labs3.jpg",
-            "/images/labs4.jpg",
-            "/images/labs5.jpg",
-            "/images/labs6.jpg"
+            "images/labs1.jpg",
+            "images/labs2.jpg",
+            "images/labs3.jpg",
+            "images/labs4.jpg",
+            "images/labs5.jpg",
+            "images/labs6.jpg"
         ];
     }
     else if (type === "events") {
         images = [
-            "/images/event1.jpg",
-            "/images/event2.jpg",
-            "/images/event3.jpg",
-            "/images/event4.jpg",
-            "/images/event5.jpg",
-            "/images/event6.jpg"
+            "images/event1.jpg",
+            "images/event2.jpg",
+            "images/event3.jpg",
+            "images/event4.jpg",
+            "images/event5.jpg",
+            "images/event6.jpg"
         ];
     }
     else if (type === "close") {
         text = [
-            "soham namdev"
+            "Thank You"
         ]
     }
 
@@ -137,4 +137,5 @@ function showGallery(type) {
         gallery.appendChild(imageElement);
     });
 }
+
 
